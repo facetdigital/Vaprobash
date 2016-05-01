19a3b4d1a9886ea011ad6d4e7338d33a6072a5e5
Scott W. Bradley <scottwb@scottwb.net>
2016-05-01 11:58:52 -0700

    Add some debug output to nginx-proxies.sh

--- scripts/nginx-proxies.sh
+++ scripts/nginx-proxies.sh
@@ -5,10 +5,15 @@ public_folder="/vagrant"
 
 for proxy in "$@"
 do
+  echo "------------------------------------------------------------"
+  echo "Creating Nginx Proxy: $proxy"
   hostname=${proxy%%:*}
   port=${proxy#*:}
   # Create Nginx Server Proxy Block and enable it
+  cmd="sudo ngxcp -d $public_folder -n $hostname -s $hostname -p $port -e"
+  echo "Running command: $cmd"
   sudo ngxcp -d $public_folder -n $hostname -s $hostname -p $port -e
+  echo "------------------------------------------------------------"
 done
 
 sudo service nginx restart
