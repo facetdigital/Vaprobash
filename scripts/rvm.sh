4c78cb885c4ab6800de56288858238a483e0d934
Scott W. Bradley <scottwb@scottwb.net>
2016-05-01 14:42:14 -0700

    Rework rvm trust/ignore to run in a vagrant user login shell

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -64,7 +64,5 @@ fi
 sudo usermod -a -G rvm vagrant
 
 # Automatically ignore the .rvmrc warning in our app project directory.
-rvm rvmrc warning ignore /vagrant/.rvmrc
-
 # Automatically trust any .rvmrc in our app project directory.
-rvm rvmrc trust /vagrant/.rvmrc
+su -l vagrant -c "rvm rvmrc warning ignore /vagrant/.rvmrc && rvm rvmrc trust /vagrant/.rvmrc"
