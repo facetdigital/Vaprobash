ad130a4148db9c96cac5db762ab3aad1a5ccf762
Scott W. Bradley <scottwb@scottwb.net>
2016-05-01 15:47:07 -0700

    Add some debug logging to app-rails provisioner

--- scripts/app-rails.sh
+++ scripts/app-rails.sh
@@ -2,12 +2,22 @@
 
 echo ">>> Initializing Rails App"
 
+echo ">>>>>>>>>> sourcing rvm"
+. /usr/local/rvm/scripts/rvm
+echo ">>>>>>>>>> sourcing rvm.sh"
+. /etc/profile.d/rvm.sh
+
 # Ignore the .rvmrc warning in our app project directory.
+echo ">>>>>>>>>> ignoring warning"
 rvm rvmrc warning ignore /vagrant/.rvmrc
 
 # Trust any .rvmrc in our app project directory.
+echo ">>>>>>>>>> trusting rvmrc"
 rvm rvmrc trust /vagrant/.rvmrc
 
+echo ">>>>>>>>>> cd-ing into /vagrant"
 cd /vagrant
+echo ">>>>>>>>>> running bundle install"
 bundle install
+echo ">>>>>>>>>> running rake db:setup"
 rake db:setup
