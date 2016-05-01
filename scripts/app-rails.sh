1e4c2b7609e3d1f5bddc8d58a8e0d1608c02e942
Scott W. Bradley <scottwb@scottwb.net>
2016-05-01 16:00:18 -0700

    See if sourcing /vagrant/.rvmrc helps

--- scripts/app-rails.sh
+++ scripts/app-rails.sh
@@ -2,27 +2,16 @@
 
 echo ">>> Initializing Rails App"
 
-echo ">>>>>>>>>> whoami:"
-whoami
-
-echo ">>>>>>>>>> which rvm:"
-which rvm
-
-echo ">>>>>>>>>> sourcing rvm"
-. /usr/local/rvm/scripts/rvm
-echo ">>>>>>>>>> sourcing rvm.sh"
-. /etc/profile.d/rvm.sh
-
 # Ignore the .rvmrc warning in our app project directory.
-echo ">>>>>>>>>> ignoring warning"
 rvm rvmrc warning ignore /vagrant/.rvmrc
 
 # Trust any .rvmrc in our app project directory.
-echo ">>>>>>>>>> trusting rvmrc"
 rvm rvmrc trust /vagrant/.rvmrc
 
 echo ">>>>>>>>>> cd-ing into /vagrant"
 cd /vagrant
+echo ">>>>>>>>>> sourcing .rvmrc"
+source .rvmrc
 echo ">>>>>>>>>> running bundle install"
 bundle install
 echo ">>>>>>>>>> running rake db:setup"
