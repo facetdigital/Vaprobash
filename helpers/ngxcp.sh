066e5b08fc7746274d1aa48269363e1aaae3e2f0
Scott W. Bradley <scottwb@scottwb.net>
2016-05-01 12:05:23 -0700

    Add missing -p arg requirment to ngxcp

--- helpers/ngxcp.sh
+++ helpers/ngxcp.sh
@@ -106,7 +106,7 @@ UpstreamPort=3000
 # - Run it in "silence"-mode by starting with a ":"
 # - Single ":" after an argument means "required"
 # - Double ":" after an argument means "optional"
-while getopts ":hd:s:n::ef" OPTION; do
+while getopts ":hd:s:n:p::ef" OPTION; do
     case $OPTION in
         h)
             show_usage
