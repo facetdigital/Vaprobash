13d49c63163eb6f602617678529840d90d656220
Jeremy Groh <jgroh9@users.noreply.github.com>
2017-05-04 10:02:42 -0400

    Update Couchbase Installation & PHP version Installation

--- scripts/couchbase.sh
+++ scripts/couchbase.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+PHP_VERSION=$1
+
 echo ">>> Installing Couchbase Server"
 
 # Set some variables
 COUCHBASE_EDITION=community
-COUCHBASE_VERSION=2.2.0 # Check http://http://www.couchbase.com/download/ for latest version
-COUCHBASE_ARCH=x86_64
+COUCHBASE_VERSION=4.0.0 # Check http://http://www.couchbase.com/download/ for latest version
+COUCHBASE_ARCH=ubuntu14.04_amd64
 
 
 wget --quiet http://packages.couchbase.com/releases/${COUCHBASE_VERSION}/couchbase-server-${COUCHBASE_EDITION}_${COUCHBASE_VERSION}_${COUCHBASE_ARCH}.deb
@@ -18,7 +20,7 @@ PHP_IS_INSTALLED=$?
 dpkg -s php-pear
 PEAR_IS_INSTALLED=$?
 
-dpkg -s php5-dev
+dpkg -s php-dev
 PHPDEV_IS_INSTALLED=$?
 
 if [ ${PHP_IS_INSTALLED} -eq 0 ]; then
@@ -28,20 +30,21 @@ if [ ${PHP_IS_INSTALLED} -eq 0 ]; then
     fi
 
     if [ ${PHPDEV_IS_INSTALLED} -eq 1 ]; then
-        sudo apt-get -qq install php5-dev
+        sudo apt-get -qq install php-dev
     fi
 
-    sudo wget --quiet -O/etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com/ubuntu/couchbase-ubuntu1204.list
+    sudo wget --quiet -O/etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com/ubuntu/couchbase-ubuntu1404.list
     wget --quiet -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key add -
     sudo apt-get update
-    sudo apt-get -qq install libcouchbase2-libevent libcouchbase-dev
+    sudo apt-get -qq install libcouchbase-dev build-essential zlib1g-dev
 
-    sudo pecl install couchbase-1.2.2
-    sudo cat > /etc/php5/mods-available/couchbase.ini << EOF
+    sudo pecl install pcs-1.3.3 # since couchbase-2.2.4
+    sudo pecl install couchbase
+    sudo cat > /etc/php/${PHP_VERSION}/mods-available/couchbase.ini << EOF
 ; configuration for php couchbase module
 ; priority=30
 extension=couchbase.so
 EOF
-    sudo php5enmod couchbase
-    sudo service php5-fpm restart
+    sudo phpenmod couchbase
+    sudo service php${PHP_VERSION}-fpm restart
 fi
