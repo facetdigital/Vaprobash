468244bc60ef98dc8b60ab98dfb2730f76c98b87
Andrew Libby <alibby@andylibby.org>
2020-06-16 13:52:30 -0400

    Increase ssl key from 1024 to 2048 to appease nginx.

--- scripts/base.sh
+++ scripts/base.sh
@@ -41,7 +41,7 @@ emailAddress=
 
 sudo mkdir -p "$SSL_DIR"
 
-sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 1024
+sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 2048
 sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.csr" -passin pass:$PASSPHRASE
 sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.crt"
 
